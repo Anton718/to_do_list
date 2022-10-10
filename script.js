b78057cb84a59e2933cbf50491199dd87998d677
Silingardi1 <anton.kostroma@yahoo.com>
2022-10-10 06:33:08 -0400

    add input val

--- script.js
+++ script.js
@@ -11,20 +11,17 @@ function getInput() {
     return val;
 }
 
-
-
 button.addEventListener('click', create_task)
 
 
 function create_task() {
-        
-        const div_task = document.createElement('div')
-        div_task.style.padding = '24px'
-        const input_checkbox = document.createElement('input')
-        input_checkbox.type = 'checkbox'
-        input_checkbox.style.cursor = 'pointer'
-        const p = document.createElement('p')
-    p.textContent = getInput();
+    const div_task = document.createElement('div')
+    div_task.style.padding = '24px'
+    const input_checkbox = document.createElement('input')
+    input_checkbox.type = 'checkbox'
+    input_checkbox.style.cursor = 'pointer'
+    const p = document.createElement('p')
+    p.textContent = getInput()
     p.style.width = '500px'
     p.style.textAlign = 'left'
     input_checkbox.addEventListener('input', cross_line)
@@ -32,48 +29,48 @@ function create_task() {
         if (input_checkbox.checked) {
             p.style.textDecoration = 'line-through'; p.style.opacity = '0.5';
             input_checkbox.style.backgroundColor = '#7749F8';
-        } else
-        {
-            p.style.textDecoration = 'none'; p.style.opacity = '1'; 
+        } else {
+            p.style.textDecoration = 'none'; p.style.opacity = '1';
             input_checkbox.style.backgroundColor = 'none';
         }
         
     }
-        const button_cross = document.createElement('button')
-        button_cross.textContent = '❌'
-        button_cross.style.border = 'none'
-        button_cross.style.backgroundColor = '#ffffff'
-        button_cross.style.cursor = 'pointer'
-        div_task.style.display = 'flex'
+    const button_cross = document.createElement('button')
+    button_cross.textContent = '❌'
+    button_cross.style.border = 'none'
+    button_cross.style.backgroundColor = '#ffffff'
+    button_cross.style.cursor = 'pointer'
+    div_task.style.display = 'flex'
     div_task.style.justifyContent = 'space-between'
-        if (input.value === "") {
-            button.removeEventListener(); 
+    if (input.value === "") {
+        button.removeEventListener();
            
-        }
+    }
     
-        if (input !== 0) { list_delete.style.display = 'flex'; }
+    if (input !== 0) { list_delete.style.display = 'flex'; }
     
-btn_del_all.addEventListener('click', del_all)
-function del_all() {
-    div.innerHTML = '';
-    list_delete.style.display = 'none'
-}
-        btn_del_complete_tasks.addEventListener('click', del_task)
-        function del_task() { 
-    if (input_checkbox.checked) {div_task.outerHTML = ""}
-}
-        button_cross.addEventListener('click', del_cross)
-        function del_cross() {
-            div_task.outerHTML = ""
-    }  
+    btn_del_all.addEventListener('click', del_all)
+    function del_all() {
+        div.innerHTML = '';
+        list_delete.style.display = 'none'
+    }
+    btn_del_complete_tasks.addEventListener('click', del_task)
+    function del_task() {
+        if (input_checkbox.checked) { div_task.outerHTML = "" }
+    }
+    button_cross.addEventListener('click', del_cross)
+    function del_cross() {
+        div_task.outerHTML = ""
+    }
+    input.value =''
     
-    
-        div_task.append(input_checkbox)
-        div_task.append(p)
-        div_task.append(button_cross)
-        div.append(div_task)
+    div_task.append(input_checkbox)
+    div_task.append(p)
+    div_task.append(button_cross)
+    div.append(div_task)
     return div
 }
+
 
 
 
